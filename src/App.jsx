d9a988a4cbc969e2373f0a6bc1cbb73c367234cd
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-12-06 11:33:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Albums from './components/Albums'
+import NewAlbumForm from './components/NewAlbumForm'
+
+
+function App() {
+  return (
+    <div>
+    <Albums/>
+    <NewAlbumForm />
+    </div>
+  )
+}
+
+export default App
